d31d894f060a9f08c6d7544aed93ef5ccc4f02a9
Pranav Marathe <pmarathe25@gmail.com>
2019-04-13 22:42:55 -0700

    Moves node tests into test_graph

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,6 +1,26 @@
 from srbuild.graph.graph import Graph
 from srbuild.graph.node import Node
-from test_node import diamond_graph
+
+def linear_graph():
+    # Constructs a linear graph:
+    # A -> B -> C
+    A = Node(path="A")
+    B = Node(inputs=[A], path="B")
+    C = Node(inputs=[B], path="C")
+    return A, B, C
+
+def diamond_graph():
+    # Constructs a diamond shaped graph:
+    #     A
+    #   /  \
+    # B     C
+    #   \  /
+    #    D (output)
+    A = Node(path="A")
+    B = Node(inputs=[A], path="B")
+    C = Node(inputs=[A], path="C")
+    D = Node(inputs=[B, C], path="D")
+    return A, B, C, D
 
 def multitier_graph():
     #   A
@@ -29,3 +49,29 @@ class TestGraph(object):
         assert all([len(layer) == 1 for layer in layers])
         # Test get functionality
         assert graph["C"].path == "C"
+
+class TestNodes(object):
+    def test_linear_outputs_correct(self):
+        A, B, C = linear_graph()
+        assert B in A.outputs
+        assert C in B.outputs
+
+    def test_diamond_outputs_correct(self):
+        A, B, C, D = diamond_graph()
+        assert B in A.outputs and C in A.outputs
+        assert D in B.outputs
+        assert D in C.outputs
+
+    def test_hashes_same_node(self):
+        A0 = Node(path="A")
+        A1 = A0
+        assert hash(A0) == hash(A1)
+        test = set([A0, A1])
+        assert len(test) == 1
+
+    def test_hashes_different_node(self):
+        A = Node(path="A")
+        B = Node(path="B")
+        assert hash(A) != hash(B)
+        test = set([A, B])
+        assert len(test) == 2
