baf9ff13c87ce3ce73b972219271c63fc0c306bf
Pranav Marathe <pmarathe25@gmail.com>
2019-06-27 21:57:38 -0700

    Fixes long description content type in setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     version=sbuildr.__version__,
     description="A python-based meta-build system for C++ projects.",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Pranav Marathe",
     author_email="pmarathe25@gmail.com",
     python_requires=">=3",
