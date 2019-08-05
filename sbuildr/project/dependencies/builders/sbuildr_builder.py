95a8e4bdd0db033adb19b27026e0fafbe724d734
Pranav Marathe <pmarathe25@gmail.com>
2019-08-04 20:46:16 -0700

    SBuildrBuilder implementation

---
+++ sbuildr/project/dependencies/builders/sbuildr_builder.py
@@ -0,0 +1,23 @@
+from sbuildr.project.dependencies.builders.builder import DependencyBuilder
+from sbuildr.misc import utils
+from sbuildr.logger import G_LOGGER
+import subprocess
+import sys
+
+class SBuildrBuilder(DependencyBuilder):
+    def __init__(self, build_script_name: str="build.py"):
+        """
+        Builds projects using the SBuildr build system.
+
+        :param build_script_name: The name of the build script. Defaults to "build.py".
+        """
+        self.build_script_name = build_script_name
+
+    def setup(self, source_dir: str, header_dir: str, lib_dir: str, exec_dir: str):
+        # Configure, then install the project.
+        configure_status = subprocess.run([sys.executable, self.build_script_name, "configure"], capture_output=True, cwd=source_dir)
+        if configure_status.returncode:
+            G_LOGGER.critical(f"Failed to configure dependency in {source_dir} with:\n{utils.subprocess_output(configure_status)}")
+        install_status = subprocess.run([sys.executable, self.build_script_name, "install", "-I", header_dir, "-L", lib_dir, "-X", exec_dir], capture_output=True, cwd=source_dir)
+        if install_status.returncode:
+            G_LOGGER.critical(f"Failed to install dependency from {source_dir} with:\n{utils.subprocess_output(install_status)}")
--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,16 +1,37 @@
 from sbuildr.project.dependencies.fetchers.git_fetcher import GitFetcher
+from sbuildr.project.dependencies.builders.sbuildr_builder import SBuildrBuilder
+from sbuildr.misc import paths
+from test_tools import PATHS, ROOT, TESTS_ROOT
 
 import tempfile
 import pytest
 import os
 
-def test_can_fetch_stest_repo():
-    commit_hash = "b2dd61e5669f9a2dee75d55eaf2950a722eebeae"
-    fetcher = GitFetcher(url="https://github.com/pmarathe25/SLog.git", commit=commit_hash)
+class TestGitFetcher(object):
+    def setup_method(self):
+        self.commit_hash = "b2dd61e5669f9a2dee75d55eaf2950a722eebeae"
+        self.fetcher = GitFetcher(url="https://github.com/pmarathe25/SLog.git", commit=self.commit_hash)
 
-    with tempfile.TemporaryDirectory() as install_dir:
-        dep_info = fetcher.fetch(install_dir)
-        assert dep_info.name == "SLog"
-        assert dep_info.path == os.path.join(install_dir, "SLog")
-        assert dep_info.version_tags == [commit_hash]
-        assert os.path.exists(dep_info.path)
+    def test_can_fetch_stest_repo(self):
+        with tempfile.TemporaryDirectory() as install_dir:
+            dep_info = self.fetcher.fetch(install_dir)
+            assert dep_info.name == "SLog"
+            assert dep_info.path == os.path.join(install_dir, "SLog")
+            assert dep_info.version_tags == [self.commit_hash]
+            assert os.path.exists(dep_info.path)
+
+class TestSBuildrBuilder(object):
+    def setup_method(self):
+        self.builder = SBuildrBuilder()
+        self.output_dir = tempfile.TemporaryDirectory()
+        self.header_dir = os.path.join(self.output_dir.name, "include")
+        self.lib_dir = os.path.join(self.output_dir.name, "lib")
+        self.exec_dir = os.path.join(self.output_dir.name, "bin")
+        os.makedirs(self.header_dir)
+        os.makedirs(self.lib_dir)
+        os.makedirs(self.exec_dir)
+
+    def test_can_build_example_repo(self):
+        self.builder.setup(ROOT, self.header_dir, self.lib_dir, self.exec_dir)
+        assert os.path.exists(os.path.join(self.header_dir, "math.hpp"))
+        assert os.path.exists(os.path.join(self.lib_dir, paths.libname("math")))
